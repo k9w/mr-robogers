76ab223ed12ffde1279db0cc44346c3fb8d93671
Kevin Williams <kevin@k9w.org>
2022-01-30 17:08:41 -0800

    Reset page after submit. Show alert to let user see output before it goes away.

--- js/scripts.js
+++ js/scripts.js
@@ -20,11 +20,14 @@ function getNumberRange(inputtedNumber) {
 $(document).ready(function() {
   $("form#enterANumber").submit(function(event) {
     event.preventDefault();
+    let defaultDomState = document.body.innerHTML;
     console.log(parseInt($("#numberInput").val()));
     let numberRange = getNumberRange(parseInt($("#numberInput").val()));
     console.log(numberRange);  
     $("#result").show();
     $("#result").append(numberRange);
+    alert("Read the result on the page. It disappears after you dismiss this alert.");
+    document.body.innerHTML = defaultDomState;
   });
 });
 
