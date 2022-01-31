807d33e8219934b77b784983ca7866e2ff7394ca
Kevin Williams <kevin@k9w.org>
2022-01-30 22:39:59 -0800

    Plan out how to convert numbers with 1, 2, or 3 to words.

--- js/scripts.js
+++ js/scripts.js
@@ -9,9 +9,40 @@ function getNumberRange(inputtedNumber) {
   for (let i = 0; i < inputtedNumber + 1; i++) {
     array.push(i);
   }
+  convertNumbersContaining1s2s3sToStringOfWords(array);
   return array;
 }
 
+/*
+
+pseudocode
+
+for each element in the array
+
+convert it from number to string and save it in a new array of strings
+(leave the original array unmodified)
+
+check if string contains 1, 2, or 3.
+
+if it contains a 1, change the whole string from the number to:
+"Beep!"
+
+if it contains a 2, change the whole string from the number to:
+"Boop!"
+
+if it contains a 3, change the whole string from the number to:
+"Won't you be my neighbor?"
+
+function convertNumbersContaining1s2s3sToStringOfWords(array) {
+  
+}
+
+function doesNumberContain1s2s3s(array) {
+  let string = element.toString();
+  string.includes(word)
+}
+
+*/
 
 
 
