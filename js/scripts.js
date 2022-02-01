c1f35a0161d347d2e86c3dd19e41538cc126941e
Kevin Williams <kevin@k9w.org>
2022-02-01 00:01:34 -0800

    Remove newer console.log statements.

--- js/scripts.js
+++ js/scripts.js
@@ -7,12 +7,14 @@ function getNumberRange(inputtedNumber) {
     let element = array[i];
     if (element.toString().includes("3") === true) {
       element = "Won't you be my neighbor?";
+    console.log('true');
     } else if (element.toString().includes("2") === true) {
       element = "Boop!";
     } else if (element.toString().includes("1") === true) {
       element = "Beep!";
     }
   }
+  return array;
 }
 
 
@@ -23,7 +25,6 @@ $(document).ready(function() {
     event.preventDefault();
     $("#result").empty();
     let numberRange = getNumberRange(parseInt($("#numberInput").val()));
-    numberRange.forEach(element => console.log(element));
     $("#result").show();
     $("#result").append(numberRange.join(", "));
   });
