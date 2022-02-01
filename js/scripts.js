e3ae04174155cde556ab69c7fc65fd3876253a23
Kevin Williams <kevin@k9w.org>
2022-01-31 22:17:06 -0800

    Get Business Logic working.

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,20 @@
 /* Business Logic */
 
-
 function getNumberRange(inputtedNumber) {
   const array = [];
   for (let i = 0; i < inputtedNumber + 1; i++) {
     array.push(i);
-  }
-
-/* The for loop below is suppsed to check for elements in the array
- * containing 3s, 2s, or 1s, in that order of priority. But it fails
- * at the first opening if condition because 'element.includes' is not
- * a function. I don't yet know how to fix that. If you comment out
- * the entire for loop below, the numbers will display correctly on
- * the rendered webpage, just not with the word substituions requested
- * in the project prompt. I could use some help with this.
- */
-
-  for (let element of array) {
-    if (element.toString.includes("3") === true) {
-      // convert number containing 3 to "Won't you be my neighbor?"
+    let element = array[i];
+    if (element.toString().includes("3") === true) {
       element = "Won't you be my neighbor?";
-    } else if (element.toString.includes("2") === true) {
-      // convert number containing 2 to "Boop!"
+    } else if (element.toString().includes("2") === true) {
       element = "Boop!";
-    } else if (element.toString.includes("1") === true) {
-      // convert number containing 1 to "Beep!"
+    } else if (element.toString().includes("1") === true) {
       element = "Beep!";
     }
   }
-
-  return array;
 }
+
 
 /* User Interface Logic */
 
@@ -44,4 +28,3 @@ $(document).ready(function() {
     $("#result").append(numberRange.join(", "));
   });
 });
-
